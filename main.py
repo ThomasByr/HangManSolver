4b2ab6bec0b04c3d4d871ef6cc156281e483bdd6
Thomas Byr <tbouyer2000@gmail.com>
2021-07-22 20:41:19 +0200

    python main

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from core.search import *
+from core.user_space import *
 
-msg = "---user input---\nformat : letters or dots then tested letters\nfor eg : .a.er aeiouls\n\n_"
+msg = u"---user input---\nformat : letters or dots then tested letters\nfor eg : .a.er aeiouls\n\n_"
 
 #! put path below
 with open("assets\\fr_dict.txt", "r", encoding="utf-8") as f:
     d = f.readlines()  # list of lines
     d = [x.strip().lower() for x in d]  # lowercase w/o spaces
 
-if __name__ == "__main__":
-    user_space = input(msg).lower()
-    print(find(user_space, d))
+if __name__ == "__main__":  # main
+    user_space = input(msg).lower()  # first time input
+    prev = user_space  # save the previous input
+    prettyprint(user_space, d)  # pretty print
+
+    # type "quit()" to exit
+    while user_space != "quit()":
+        user_space = input("\n_").lower()  # next time input
+        if user_space[0] == "+":  # if + then add
+            # look for space in previous input then add it
+            user_space = f"{prev} {user_space[1:]}" if " " not in prev else f"{prev}{user_space[1:]}"
+        try:
+            if (i := int(user_space[0])) in range(100):  # if int then add
+                n = len(user_space) - 1
+                # look for position in previous input then replace it
+                user_space = f"{prev[:i]}{user_space[1:]}{prev[i+n:]}"
+        except ValueError:
+            pass  # do nothing
+        prettyprint(user_space, d)
